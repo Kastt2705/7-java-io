97a2582d203982e442c2dd62b03ad20b3d5ddb7a
Kastt2705 <zhenya.litovchenko.2000@mail.ru>
2024-11-07 17:43:48 +0500

    Lab7

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,30 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
-public class Task04Main {
-    public static void main(String[] args) throws IOException {
+public class Task04Main
+{
+    public static void main(String[] args) throws IOException
+    {
         // чтобы протестировать свое решение, вам нужно:
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        //System.setIn(new StringBufferInputStream("a1 b2 c3"));
+    }
+    private  static void SumFloat(InputStream stream)
+    {
+        double sum = 0.0;
+        DecimalFormat df = new DecimalFormat("0.000000");
 
-        System.out.println("0.0");
+        try (Scanner scanner = new Scanner(stream)) {
+            while (scanner.hasNextDouble()) {
+                sum += scanner.nextDouble();
+            }
+        }
+        System.out.print(df.format(sum));
     }
 }
